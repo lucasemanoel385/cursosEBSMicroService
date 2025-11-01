edfaf29718c033d69fcb50b5b8229fc10458b675
lucasemanoel385 <lucas.emanoelbastossilva@gmail.com>
2025-10-31 23:47:17 -0300

    fix: adjusting

--- enrollment-service/src/main/java/br/com/cursosEBS/enrollments/controller/SubscriptionController.java
+++ enrollment-service/src/main/java/br/com/cursosEBS/enrollments/controller/SubscriptionController.java
@@ -4,6 +4,7 @@ import br.com.cursosEBS.enrollments.dto.RegisterSubscriptionDTO;
 import br.com.cursosEBS.enrollments.dto.UpdateStatusSubscriptionDTO;
 import br.com.cursosEBS.enrollments.entity.Subscription;
 import br.com.cursosEBS.enrollments.service.SubscriptionService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +18,9 @@ public class SubscriptionController {
     private SubscriptionService subscriptionService;
 
     @PostMapping
-    public ResponseEntity<RegisterSubscriptionDTO> registerSubscription(@RequestBody RegisterSubscriptionDTO dto, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<RegisterSubscriptionDTO> registerSubscription(@RequestBody @Valid RegisterSubscriptionDTO dto, UriComponentsBuilder uriBuilder) {
 
         var subs = subscriptionService.registerSubscription(dto);
-        System.out.println(subs.id() + " -- " + subs.userId());
         var uri = uriBuilder.path("/subscription/{id}").buildAndExpand(subs.id()).toUri();
 
         return ResponseEntity.created(uri).body(subs);
